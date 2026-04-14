0beb13d8130015a772823125392b435c8b52a11b
HatimM09 <321hatim@gmail.com>
2026-04-14 18:17:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -404,7 +404,7 @@ function HomePage({ setActiveTab }) {
   const [statsLoading, setStatsLoading] = useState(true)
   const [paymentError, setPaymentError] = useState('')
 
-  const gpayReceiverUpiId = '9876543210@upi'
+  const gpayReceiverUpiId = '6375250267@upi'
   const fixedPaymentAmount = '400.00'
 
   const surveyOpen = isSurveyOpen()
