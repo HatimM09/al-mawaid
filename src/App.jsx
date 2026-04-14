268b253e8ce4a6888797398ab09311bde5d8f35c
HatimM09 <321hatim@gmail.com>
2026-04-14 12:03:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -395,7 +395,7 @@ function HomePage({ setActiveTab }) {
   const [paymentReceipt, setPaymentReceipt] = useState(null)
   const [paymentPending, setPaymentPending] = useState(false)
 
-  const gpayReceiverUpiId = '9876543210@upi'
+  const gpayReceiverUpiId = 'murtazacool558@okhdfcbank'
   const fixedPaymentAmount = '400.00'
   const paymentStorageKey = `almawaid_payment_receipt_${user.id}`
 
